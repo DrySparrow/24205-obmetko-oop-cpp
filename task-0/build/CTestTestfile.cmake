4611b4e5461b83948000c6ad98049a8e5f3ea12c
DrySparrow <obmetko.art@mail.ru>
2025-12-15 13:34:55 +0700

    initial task-0

---
+++ task-0/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Sergey/Desktop/coding/tasks_oop/task-0
+# Build directory: C:/Users/Sergey/Desktop/coding/tasks_oop/task-0/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(AllTests "C:/Users/Sergey/Desktop/coding/tasks_oop/task-0/build/run_all_tests.exe")
+set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Sergey/Desktop/coding/tasks_oop/task-0/CMakeLists.txt;68;add_test;C:/Users/Sergey/Desktop/coding/tasks_oop/task-0/CMakeLists.txt;0;")
